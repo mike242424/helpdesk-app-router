d8891e00711c297322406f02566b283fea39f476
Michael Byers <mikebyers24@gmail.com>
2024-04-15 11:22:53 -0400

    created a route group called auth to structure login and signup

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <main>
+      <h1 className="text-center">Log In</h1>
+    </main>
+  );
+}
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function Signup() {
+  return (
+    <main>
+      <h1 className="text-center">Sign Up</h1>
+    </main>
+  );
+}
